50057ede2dbbd709fa47c2b83630b82522dadf75
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-02-26 13:13:19 -0600

    Se refactoriza, eliminado el código repetido.

--- day1.js
+++ day1.js
@@ -12,39 +12,18 @@ let stringTreinta = '30'
 let numeroDiez = 10
 let stringDiez = '10'
 
+
+// Comparar variables
+// Función que compara dos variables y muestra un mensaje en consola  
+// si los valores son iguales o diferentes
 function CompararVariables(var1, var2) {
-  if (var1 === var2) {
-    console.log(`Las variables ${var1} y ${var2} tienen el mismo valor y el mismo tipo`)
-  } else {
-    console.log(`Las variables ${var1} y ${var2} no tienen el mismo tipo`)
+  if (var1 == var2) {
+    console.log(`Las variables ${var1} tipo ${typeof(var1)} y "${var2}" tipo ${typeof(var2)} tienen el mismo valor, pero tipos difernetes`)
+    } else {
+    console.log(`Las variables ${var1} y ${var2} no tienen el mismo valor`)
   }
 }
 
 CompararVariables(numeroUn, stringUn)
 CompararVariables(numeroTreinta, stringTreinta)
 CompararVariables(numeroDiez, stringDiez)
-
-
-
-// Comparar numeroUn y stringUn
-if (numeroUn == stringUn) {
-  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
-} else {
-  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
-}
-
-
-// Comparar numeroTreinta y stringTreinta
-if (numeroTreinta == stringTreinta) {
-  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
-} else {
-  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
-}
-
- 
-// Comparar numeroDiez y stringDiez
-if (numeroDiez == stringDiez) {
-  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
-} else {
-  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
-}
