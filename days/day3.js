474ac3b33358816c3cb95d2c06ef5a287d03ec70
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-03-01 18:38:42 -0600

    Día 4.

--- days/day3.js
+++ days/day3.js
@@ -8,18 +8,22 @@ usuario pueda elegir:
 
 - Si quiere seguir hacia el área de Front-End o seguir hacia el área de
   Back-End.
+
 - Si está en el área de Front-End, si quiere aprender React o aprender Vue. Si
   está en el área de Back-End, podrá aprender C# o aprender Java.
+
 - Después, independientemente de las elecciones anteriores, el usuario podrá
   elegir entre seguir especializándose en el área elegida o desarrollarse para
   convertirse en Fullstack. Debes mostrar en pantalla un mensaje específico para
   cada elección.
+
 - Finalmente, pregunta en qué tecnologías le gustaría a la persona
-  especializarse o conocer. Aquí, la persona puede responder N tecnologías, una
-  a la vez. Entonces, mientras continúe respondiendo **ok** a la pregunta: "¿Hay
-  alguna otra tecnología que te gustaría aprender?", sigue presentando el
-  Prompt, para que complete el nombre de la tecnología en cuestión. Y, justo
-  después, presenta un mensaje comentando algo sobre la tecnología ingresada.
+  especializarse o conocer. Aquí, la persona puede responder N
+  tecnologías, una a la vez. Entonces, mientras continúe respondiendo
+  **ok** a la pregunta: "¿Hay alguna otra tecnología que te gustaría
+  aprender?", sigue presentando el Prompt, para que complete el nombre
+  de la tecnología en cuestión. Y, justo después, presenta un mensaje
+  comentando algo sobre la tecnología ingresada.
 
 Lo importante es que la persona que esté jugando siempre pueda elegir qué
 decisión tomar para aprender y desarrollarse en el área de programación.
---
+++ days/day4.js
@@ -0,0 +1,44 @@
+/* ** [[ Día 4 - Cíclos y Repeticiones ]] ** */
+
+// import { number } from '@inquirer/prompts';
+import number from '@inquirer/number';
+
+const description = `
+Debes crear un  pequeño programa que comience con  un valor específico
+predefinido  entre 0  y 10  para  el número  que vas  a adivinar  (por
+ejemplo, el 7).
+
+A continuación, el programa te preguntará  cuál es el valor que deseas
+adivinar y,  si aciertas, te  felicitará. Si  te equivocas, te  dará 2
+intentos más.
+
+Al final,  si no aciertas en  ninguno de los intentos,  imprimirá cuál
+era el número inicial.
+
+Después de  que el programa  esté funcionando, intenta usar  un número
+aleatorio en lugar de uno predefinido.
+`
+
+const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+let intentos = 3;
+let respuesta;
+
+const run = async () => {
+    while (intentos > 0) {
+        const respuesta = await number({ message: `Adivina el número (${numeroAleatorio})` });
+        if (respuesta === numeroAleatorio) {
+            console.log("¡Felicidades! Haz acertado.");
+            break;
+        } else {
+            intentos--; // Decrementa primero
+            if (intentos > 0) {
+                console.log(`No haz adivinado. Te quedan ${intentos} intento(s).`);
+            } else {
+                console.log(`No adivinaste. El número era ${numeroAleatorio}`);
+            }
+        }
+    }
+}
+
+export { description, run }
+
